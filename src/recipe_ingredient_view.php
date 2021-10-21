7caa770ab59bbcb1fb534cbcd9c5fe6962a681ea
Jake Day <jday@scu.edu>
2021-10-21 00:56:47 -0700

    Add divs to center recipe elements

--- src/recipe_ingredient_view.php
+++ src/recipe_ingredient_view.php
@@ -11,14 +11,15 @@ class RecipeIngredientView extends View {
     $select = $this->select(["recipe_id" => $id], "i");
     $recipe_ingr_obj = json_decode($select);
 
-    $html = '<div class="ingredients"><p id = "ing">Ingredients</p><ul>';
+    $html = '<div class="ingredients"><div class="ingredients inner">';
+    $html .= '<p id = "ing">Ingredients</p><ul>';
     
     foreach($recipe_ingr_obj as $ingr) {
       //echo "- " . $ingr->amount . " " . $ingr->ingredient . "<br>";
       $html .= '<li><input type = "checkbox" id = "ingredients">'.$ingr->amount.' '.$ingr->ingredient.'</li><br/>';
     }
 
-    $html .= '</ul></div>';
+    $html .= '</ul></div></div>';
     echo $html;
   }
 }
